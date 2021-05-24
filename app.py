e1a256e682585e2eb9ca9fe815af9d51f925bd94
Hyunsu <hyunsujang1227@gmail.com>
2021-05-24 17:45:47 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,13 @@ app = Flask(__name__)
 def index():
     return "hello wolrd!"
 
+# if __name__ == '__main__':
+
+#     listen_port = '2000'
+
+#     ipaddr=subprocess.getoutput("hostname -I").split()[0]
+#     print ("Starting the service with ip_addr="+ipaddr)
+#     app.run(debug=True,host=ipaddr,port=int(listen_port),threaded=True)
+
 if __name__ == '__main__':
-
-    listen_port = '2000'
-
-    ipaddr=subprocess.getoutput("hostname -I").split()[0]
-    print ("Starting the service with ip_addr="+ipaddr)
-    app.run(debug=True,host=ipaddr,port=int(listen_port),threaded=True)
+    app.run(debug=True)
