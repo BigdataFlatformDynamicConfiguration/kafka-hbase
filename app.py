68c8a2a85493f3c749d3c04a273a678a0c3c85b2
Hyunsu <hyunsujang1227@gmail.com>
2021-05-26 21:25:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, jsonify, request, render_template
 import subprocess
 import happybase
 import sys
-from waitress import serve
 
 app = Flask(__name__)
 
@@ -81,7 +80,6 @@ if __name__ == '__main__':
     connection.open()
     print(connection)
    
-#     serve(app, host="0.0.0.0", port=2000)
     listen_port = '2000'
     app.run(debug=True, port=int(listen_port), host='0.0.0.0')
     
