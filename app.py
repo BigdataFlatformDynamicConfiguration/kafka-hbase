a8c3657f992293bbc70303815455112cc5c597e7
Hyunsu <hyunsujang1227@gmail.com>
2021-05-27 20:07:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -91,12 +91,14 @@ def row_list():
     
     producer.send('my-topic1', data)
     
-    if message in consumer:
+    for message in consumer:
         # message value and key are raw bytes -- decode if necessary!
         # e.g., for unicode: `message.value.decode('utf-8')`
         print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
                                           message.offset, message.key,
                                           message.value)) 
+        break
+        
     consumer.close()
     producer.close()
     
