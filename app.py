64731f8674154d47617c0d17a7418aa077d7920b
Hyunsu <hyunsujang1227@gmail.com>
2021-05-30 23:25:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ class Consumer(threading.Thread):
                                           message.offset, message.key,
                                           message.value))
                 
-                data = json.loads(str(message.value))
+                data = json.loads(str(message.value).replace("\'", "\""))
                 print(data)
           
                 if 'table_name' in data:
