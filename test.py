1577a4f09458681d9f9aabdb272b4987a6ce9539
Hyunsu <hyunsujang1227@gmail.com>
2021-05-30 21:37:20 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import requests
 
 URL = 'http://34.64.94.147:2000/row-list'
-# data = {'param1': 'value1', 'param2': 'value'} 
-data = 'hello'
+data = {'param1': 'value1', 'param2': 'value'} 
+# data = 'hello'
 res = requests.post(URL, data=data)
