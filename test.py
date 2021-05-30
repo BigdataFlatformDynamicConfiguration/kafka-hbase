ab4462161d36b0586d3dd2dd7fd59d890ea6fb96
Hyunsu <hyunsujang1227@gmail.com>
2021-05-30 22:01:48 +0900

    Update test.py

--- test.py
+++ test.py
@@ -3,9 +3,5 @@ import json
 
 URL = 'http://34.64.94.147:2000/row-list'
 data = {'param1': 'value1', 'param2': 'value'} 
-data = json.dumps(data)
 
-print(data)
-
-# data = 'hello'
-res = requests.post(URL, data=data)
+res = requests.post(URL, data=json.dumps(data))
