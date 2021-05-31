feb992795eb2d3aa58e9b1bac9f449e8da82a880
LeeHanSeong <jkhk7788@gmail.com>
2021-06-01 05:08:27 +0900

    Update test3.py

--- test3.py
+++ test3.py
@@ -2,3 +2,4 @@ import happybase
     connection = happybase.Connection(host="test-hbase-master", port=9090)
     connection.open()
     table = connection.table('Daegue')
+    rse = table.scan(filter="SingleColumnValueFilter('ID','BLD_NM',=,'binary:경북대학교',true,true)")
