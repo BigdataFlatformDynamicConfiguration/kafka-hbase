593facb0d883836bfa9c2c0f884328b1c80934dd
Hyunsu <hyunsujang1227@gmail.com>
2021-06-04 13:48:50 +0900

    Update test2.py

--- test2.py
+++ test2.py
@@ -4,7 +4,8 @@ import json
 URL = 'http://34.64.94.147:2000/row-list'
 # data = {'param1': 'value1', 'param2': 'value'} 
 
-data = {"table_name" : "my-topic11", "data" : [{"cf1:col1" : "5","cf1:col2" : "6",},{"cf1:col1" : "7","cf1:col2" : "8",}]}
+# data = {"table_name" : "my-topic11", "data" : [{"cf1:col1" : "5","cf1:col2" : "6",},{"cf1:col1" : "7","cf1:col2" : "8",}]}
+data = {"table_name" : "my-topic11", "datalist" : [{"rowkey":"3", "data": {"cf1:col1" : "1","cf1:col2" : "2",}},{"rowkey":"4", "data": {"cf1:col1" : "3","cf1:col2" : "4",}}]}
 # {
 #   "table_name" : "my-topic11",
 #   "data" : 
