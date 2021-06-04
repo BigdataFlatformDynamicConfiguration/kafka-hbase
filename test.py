764289d54be19d86f2abf70ab0b1282df93c91a2
Hyunsu <hyunsujang1227@gmail.com>
2021-06-04 13:46:36 +0900

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ URL = 'http://34.64.120.158:2000/put-rows'
 # data = {'param1': 'value1', 'param2': 'value'} 
 
 # data = {"table_name" : "my-topic11", "data" : [{"cf1:col1" : "1","cf1:col2" : "2",},{"cf1:col1" : "3","cf1:col2" : "4",}]}
-data = {"table_name" : "my-topic11", "datalist" : [{"rowkey":1, "data": {"cf1:col1" : "1","cf1:col2" : "2",}},{"rowkey":2, "data": {"cf1:col1" : "3","cf1:col2" : "4",}}]}
+data = {"table_name" : "my-topic11", "datalist" : [{"rowkey":"1", "data": {"cf1:col1" : "1","cf1:col2" : "2",}},{"rowkey":"2", "data": {"cf1:col1" : "3","cf1:col2" : "4",}}]}
 # {
 #   "table_name" : "my-topic11",
 #   "data" : 
